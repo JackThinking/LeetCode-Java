f74ebdc465241ac4c41e1873ea5dcdffb7867b6e
Citrix <sijieqian@foxmail.com>
2018-08-15 20:04:01 +0800

    Minimum-Path-Sum-2

--- src/medium/_64/Solution.java
+++ src/medium/_64/Solution.java
@@ -5,8 +5,7 @@ package medium._64;
  */
 public class Solution {
     public int minPathSum(int[][] grid) {
-
-        int row = grid.length;
+        /*int row = grid.length;
         int col = grid[0].length;
         int dp[][] = new int[row][col];
         dp[0][0] = grid[0][0];
@@ -33,7 +32,33 @@ public class Solution {
                 dp[i][j] = Math.min(dp[i-1][j]+grid[i][j],dp[i][j-1]+grid[i][j]);
             }
         }
-        return dp[row-1][col-1];
+        return dp[row-1][col-1];*/
+        int row = grid.length;
+        int col = grid[0].length;
+        int[][] memo = new int[row][col];
+        return helper(grid,memo,row-1,col-1);
+    }
+
+    public int helper(int[][] grid, int[][] memo, int x, int y){
+        if (x==0 && y==0){
+            return grid[0][0];
+        }
+        if (memo[x][y] != 0){
+            return memo[x][y];
+        }
+
+        int left = Integer.MAX_VALUE;
+        int up = Integer.MAX_VALUE;
+        if (y>0){
+            up = helper(grid,memo,x,y-1);
+        }
+        if (x>0){
+            left = helper(grid,memo,x-1,y);
+        }
+
+        int dist = grid[x][y]+Math.min(left,up);
+        memo[x][y] = dist;
+        return dist;
     }
 
     public static void main(String[] args) {
