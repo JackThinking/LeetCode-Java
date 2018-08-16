f0aae7d78daf76ea2fbfea8f76fbcec02a0cff72
Citrix <sijieqian@foxmail.com>
2018-08-16 19:39:27 +0800

    Palindromic-Substrings-2

--- src/medium/_647/Solution.java
+++ src/medium/_647/Solution.java
@@ -4,7 +4,7 @@ package medium._647;
  * Created by Citrix on 2018/8/16.
  */
 public class Solution {
-    int res;
+    /*int res;
     public int countSubstrings(String s) {
         int[][] memo = new int[s.length()][s.length()];
         helper(s,memo,0,s.length()-1);
@@ -42,8 +42,28 @@ public class Solution {
         if (right>0){
             helper(s,memo,left,right-1);
         }
-        return memo[left][right];
+        return memo[left][right];*/
+
+    public int countSubstrings(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+             int t1 = search(s,i,i);
+             int t2 = search(s,i,i+1);
+             sum += t1+t2;
+        }
+        return sum;
     }
+
+    public int search(String s, int left ,int right){
+        int tempsum = 0;
+        while(left>0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
+            tempsum++;
+        }
+        return tempsum;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         String nums = "fdsklf";
