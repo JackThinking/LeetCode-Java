a0e5936bb93731317b9f152716acc255398dff5a
Citrix <sijieqian@foxmail.com>
2018-08-31 19:46:07 +0800

    冒牌排序改良版

--- src/extend/SortSummary.java
+++ src/extend/SortSummary.java
@@ -11,7 +11,7 @@ public class SortSummary {
     public static void main(String[] args) {
         int[] test = {2,1,4,5,8};
         SortSummary solution = new SortSummary();
-        solution.bubblesort(test);
+        solution.bubbleSortOpt(test);
     }
 
     /*
@@ -27,7 +27,7 @@ public class SortSummary {
         list[i] = list[j];
         list[j] = temp;
     }
-    public void bubblesort(int[] list){
+    public void bubbleSort(int[] list){
         for (int i = 0; i < list.length; i++) {
             for (int j = 0; j < list.length-1; j++) {
                 if (list[j+1]<list[j]){
@@ -36,5 +36,29 @@ public class SortSummary {
             }
         }
     }
+    /*
+    * 复杂度O(n^2),当序列整体较为有序时，产生很多没必要的操作
+    * */
+
+    /*
+    * 2.冒泡排序改良版
+    * */
+    public void bubbleSortOpt(int[] list){
+        int rightside = list.length;//右边界，运算需要
+        int guard = rightside;//动态小哨兵，用来动态记录最后一次交换的坐标
+        while(guard>0){
+            rightside = guard;//右边界去上一次哨兵坐标
+            guard = 0;//哨兵默认为0
+            for (int i = 1; i < rightside; i++) {
+                if (list[i-1]>list[i]){
+                    swap(list,i-1,i);
+                    guard = i;//随着记录发生而记录
+                }
+            }
+        }
+    }
+    /*
+    * 这种做法进行了剪枝，减少了不必要的比较，在最好的情况下（已经有序），可以变为O(n)
+    * */
 }
 
