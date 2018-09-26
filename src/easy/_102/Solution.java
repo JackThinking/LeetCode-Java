0660fb4417686183caab74c58cdde64bc16167cd
CitrixQian <sijieqian@foxmail.com>
2018-09-26 14:15:53 +0800

    102

--- src/easy/_102/Solution.java
+++ src/easy/_102/Solution.java
@@ -10,19 +10,21 @@ import java.util.List;
  */
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();//不能直接用List<List<Integer>>初始化
-        helper(res,root,0);
+        List<List<Integer>> res = new ArrayList<>();
+        helper(res, root, 0);
         return res;
     }
     private void helper(List<List<Integer>> res, TreeNode node, int level){
         if (node == null){
             return;
         }
-        if (res.size()<level){
-            res.add(new ArrayList<Integer>());
+        if (level>res.size()){
+            res.add(new ArrayList<>());
         }
         res.get(level).add(node.val);
         helper(res, node.left, level+1);
         helper(res, node.right, level+1);
     }
+
+
 }
