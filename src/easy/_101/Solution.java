783e4a529e7dfbad6bee59923eb54272b7d6a8f9
CitrixQian <sijieqian@foxmail.com>
2018-09-26 10:58:53 +0800

    101

--- src/easy/_101/Solution.java
+++ src/easy/_101/Solution.java
@@ -10,10 +10,10 @@ public class Solution {
         return root == null || helper(root,root);
     }
     private boolean helper(TreeNode left, TreeNode right){
-        if (left == null || right==null){
-            return left == right;
+        if (left == null || right == null){
+            return left == right;//这个地方没记住
         }
-        if (left.val != right.val){
+        if (left.val != right.val){//这个也是
             return false;
         }
         return helper(left.left, right.right) && helper(left.right, right.left);
