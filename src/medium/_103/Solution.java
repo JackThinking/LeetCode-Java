66bdc5d305f6ef15e3fd94734188e9cf18c3e200
CitrixQian <sijieqian@foxmail.com>
2018-09-27 20:14:50 +0800

    103

--- src/medium/_103/Solution.java
+++ src/medium/_103/Solution.java
@@ -14,20 +14,18 @@ public class Solution {
         helper(res, root, 0);
         return res;
     }
-
-    private void helper(List<List<Integer>> res, TreeNode node, int level) {
-        if (node == null) {
-            return;
+    private void helper(List<List<Integer>> res, TreeNode root, int level) {
+        if (root != null) {
+            if (level > res.size()) {
+                res.add(new ArrayList<>());
+            }
+            if (level % 2 == 0) {
+                res.get(level).add(root.val);
+            } else {
+                res.get(level).add(0, root.val);
+            }
+            helper(res, root.left, level + 1);
+            helper(res, root.right, level + 1);
         }
-        if (level >= res.size()) {
-            res.add(new ArrayList<>());
-        }
-        if (level % 2 == 0) {
-            res.get(level).add(node.val);
-        } else {
-            res.get(level).add(0, node.val);//偷懒的话用add的额外用法，可以指定插入的序号，一直在0插入的话，相当于数据是逆向存的
-        }
-        helper(res, node.left, level + 1);
-        helper(res, node.right, level + 1);
     }
 }
