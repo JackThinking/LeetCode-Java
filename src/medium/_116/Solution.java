18bf08b0d66c2614b5d868e869017fd87ae3600b
CitrixQian <sijieqian@foxmail.com>
2018-09-28 16:55:08 +0800

    116

--- src/medium/_116/Solution.java
+++ src/medium/_116/Solution.java
@@ -5,9 +5,9 @@ import structure.TreeLinkNode;
 /**
  * Created by Citrix on 2018/9/28.
  */
-//你看看，自己多思考也能做出来的
+//你看看，自己多思考也能做出来的；之后用堆栈的方式再做
 public class Solution {
-    public void connect(TreeLinkNode root) {
+    /*public void connect(TreeLinkNode root) {
         if (root == null) {
             return;
         }
@@ -22,6 +22,25 @@ public class Solution {
             }
             connect(root.left);
             connect(root.right);
+        }
+    }*/
+    /*
+     * 单层连接移动,这个是自己没有想到的，感觉有点像BFS，自己的做法像DFS，巧的是判断逻辑基本上是一样的
+     * */
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode levelStart = root;
+        while (levelStart != null) {
+            TreeLinkNode cur = levelStart;
+            while (cur != null) {
+                if (cur.left != null) {
+                    cur.left.next = cur.right;
+                }
+                if (cur.right != null && cur.next != null) {
+                    cur.right.next = cur.next.left;
+                }
+                cur = cur.next;
+            }
+            levelStart = levelStart.left;
         }
     }
 }
