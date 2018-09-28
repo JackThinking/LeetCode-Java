0490fae0924ffa45aa9b94d2bb29efb8e99a8342
CitrixQian <sijieqian@foxmail.com>
2018-09-28 20:25:28 +0800

    230

--- src/medium/_230/Solution.java
+++ src/medium/_230/Solution.java
@@ -3,7 +3,6 @@ package medium._230;
 import structure.TreeNode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,7 +12,7 @@ public class Solution {
     /*
      * 第一个想法，先中序遍历得到有序数组，再按照序号取
      * */
-    public int kthSmallest(TreeNode root, int k) {
+    /*public int kthSmallest(TreeNode root, int k) {
         List<Integer> res = new ArrayList<>();
         helper(res, root);
         return res.get(k - 1);
@@ -26,5 +25,24 @@ public class Solution {
         helper(res, root.left);
         res.add(root.val);
         helper(res, root.right);
+    }*/
+    /*
+     * 其实不用完全排序，只要知道所求的index与该节点的index的大小关系，即可进行优化
+     * */
+    public int kthSmallest(TreeNode root, int k) {
+        int count = count(root.left);
+        if (k > count + 1) {
+            return kthSmallest(root.right, k - count - 1);//对于大小的判断不够熟练，不是void的话，要return的！！！
+        } else if (k <= count) {
+            return kthSmallest(root.left, k);
+        }
+        return root.val;//就是当k=count+1的时候，结束递归
+    }
+
+    private int count(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + count(node.left) + count(node.right);
     }
 }
