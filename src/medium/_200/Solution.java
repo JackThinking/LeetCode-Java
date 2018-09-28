107b18644619034d5ba6554a0012bcaf684ff840
CitrixQian <sijieqian@foxmail.com>
2018-09-28 09:53:02 +0800

    200

--- src/medium/_200/Solution.java
+++ src/medium/_200/Solution.java
@@ -3,6 +3,10 @@ package medium._200;
 /**
  * Created by Citrix on 2018/9/27.
  */
+/*
+ * 自己的想法是一个个遍历，是一的话找上下左右，无疑是暴力了，参考答案的dfs法就简单了，将找到的1进行边缘延伸
+ * 将其他1都改为0，这样的话减少了不必要的操作。之后好好思考下dfs
+ * */
 public class Solution {
     int n;
     int m;
@@ -17,7 +21,7 @@ public class Solution {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == '1') {
-                    dfsSearch(grid, i, j);
+                    dfs(grid, i, j);
                     count += 1;
                 }
             }
@@ -25,14 +29,14 @@ public class Solution {
         return count;
     }
 
-    private void dfsSearch(char[][] grid, int i, int j) {
-        if (i >= n || j >= m || i < 0 || j < 0 || grid[i][j] != '1') {
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0') {
             return;
         }
         grid[i][j] = '0';
-        dfsSearch(grid, i - 1, j);
-        dfsSearch(grid, i + 1, j);
-        dfsSearch(grid, i, j - 1);
-        dfsSearch(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
     }
 }
