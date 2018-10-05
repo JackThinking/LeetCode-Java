8bde49fee9fe86374d989c87579d632878ed1522
CitrixQian <sijieqian@foxmail.com>
2018-10-05 20:36:57 +0800

    130

--- src/hard/_130/Solution.java
+++ src/hard/_130/Solution.java
@@ -49,20 +49,14 @@ public class Solution {
         if (i < 0 || j < 0 || i > board.length - 1 || j > board[0].length - 1) {
             return;
         }
-        if (board[i][j] == 'O') {
-            board[i][j] = '*';
+        if (board[i][j] == 'X'||board[i][j] == '*') {
+            return;
         }
-        if (i > 1 && board[i - 1][j] == 'O') {
-            dfs(board, i - 1, j);
-        }
-        if (i < board.length - 2 && board[i + 1][j] == 'O') {
-            dfs(board, i + 1, j);
-        }
-        if (j > 1 && board[i][j - 1] == 'O') {
-            dfs(board, i, j - 1);
-        }
-        if (j < board[0].length - 2 && board[i][j + 1] == 'O') {
-            dfs(board, i, j + 1);
-        }
+        board[i][j] = '*';
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
+
     }
 }
