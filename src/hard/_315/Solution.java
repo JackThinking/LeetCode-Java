983c8e60ad290dac3b078c1d72993d68cb7a7b2c
CitrixQian <sijieqian@foxmail.com>
2018-10-12 20:09:08 +0800

    315

---
+++ src/hard/_315/Solution.java
@@ -0,0 +1,37 @@
+package hard._315;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Citrix on 2018/10/12.
+ */
+public class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        int[] res = new int[nums.length];
+        List<Integer> list = new ArrayList<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] = insert(list, nums[i]);
+        }
+        list.clear();
+        for (int i = 0; i < nums.length; i++) {
+            list.add(res[i]);
+        }
+        return list;
+    }
+
+    private int insert(List<Integer> list, int num) {
+        int l = 0;
+        int r = list.size() - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (list.get(m) >= num) {
+                r = m - 1;
+            } else if (list.get(m) < num) {
+                l = m + 1;
+            }
+        }
+        list.add(l, num);
+        return l;
+    }
+}
