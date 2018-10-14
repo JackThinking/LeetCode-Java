57268ec91c9e35bdedd36cc2066ad606f329eb2b
CitrixQian <sijieqian@foxmail.com>
2018-10-14 14:23:25 +0800

    617

---
+++ src/easy/_617/Solution.java
@@ -0,0 +1,28 @@
+package easy._617;
+6
+import structure.TreeNode;
+
+/**
+ * Created by Citrix on 2018/10/14.
+ */
+public class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        return helper(t1, t2);
+    }
+
+    private TreeNode helper(TreeNode t1, TreeNode t2) {
+        TreeNode node = null;
+        if (t1 == null && t2 == null) {
+            return null;
+        } else if (t1 != null && t2 != null) {
+            node = new TreeNode(t1.val + t2.val);
+            node.left = helper(t1.left, t2.left);
+            node.right = helper(t1.right, t2.right);
+        } else if (t1 == null && t2 != null) {
+            node = t2;
+        } else if (t2 == null && t1 != null) {
+            node = t1;
+        }
+        return node;
+    }
+}
---
+++ src/hard/_41/Solution.java
@@ -0,0 +1,8 @@
+package hard._41;
+
+/**
+ * Created by Citrix on 2018/10/13.
+ */
+public class Solution {
+    
+}
