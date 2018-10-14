99953ee7de3ea83f9a909f3c0f293aad974d1c7c
CitrixQian <sijieqian@foxmail.com>
2018-10-14 20:00:56 +0800

    560-easy

---
+++ src/medium/_560/Solution.java
@@ -0,0 +1,24 @@
+package medium._560;
+
+/**
+ * Created by Citrix on 2018/10/14.
+ */
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int count = 0;
+        int sum[] = new int[n + 1];
+        sum[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (sum[j] - sum[i] == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
