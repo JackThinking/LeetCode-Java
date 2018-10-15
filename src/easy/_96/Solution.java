bfd6167671b8af86754628ffd086ebedd14269e7
CitrixQian <sijieqian@foxmail.com>
2018-10-15 22:11:18 +0800

    96

---
+++ src/easy/_96/Solution.java
@@ -0,0 +1,18 @@
+package easy._96;
+
+/**
+ * Created by Citrix on 2018/10/15.
+ */
+public class Solution {
+    public int numTrees(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+}
