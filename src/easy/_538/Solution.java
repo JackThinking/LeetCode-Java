1d6e8d966ff1af9d78e785e30e2c3e2fec63090c
CitrixQian <sijieqian@foxmail.com>
2018-10-16 16:01:23 +0800

    538

--- src/easy/_538/Solution.java
+++ src/easy/_538/Solution.java
@@ -8,6 +8,10 @@ import structure.TreeNode;
 public class Solution {
     int max = 0;
 
+    /*
+    * 思路正确，执行没有经验，首先确认是后序遍历执行，然后不需要重新构建树，只需要将root.val值重新赋值即可
+    * 这种重新赋值构建树的话，return的是节点本身
+    * */
     public TreeNode convertBST(TreeNode root) {
         if (root == null) {
             return root;
