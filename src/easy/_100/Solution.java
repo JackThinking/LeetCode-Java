7e41a66c3fcebe3f1b0cce3225678c79ae22c54f
CitrixQian <sijieqian@foxmail.com>
2018-10-18 19:27:14 +0800

    100

---
+++ src/easy/_100/Solution.java
@@ -0,0 +1,26 @@
+package easy._100;
+
+import structure.TreeNode;
+
+/**
+ * Created by Citrix on 2018/10/18.
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return helper(p, q);
+    }
+
+    private boolean helper(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        }
+        if (p.val == q.val) {
+            return helper(p.left, q.left) && helper(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ src/medium/_406/Solution.java
@@ -0,0 +1,8 @@
+package medium._406;
+
+/**
+ * Created by Citrix on 2018/10/18.
+ */
+public class Solution {
+
+}
