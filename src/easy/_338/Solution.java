77a7645648f8bb765e60618dccf0fc3e763058d4
CitrixQian <sijieqian@foxmail.com>
2018-10-19 20:20:21 +0800

    338

--- src/easy/_338/Solution.java
+++ src/easy/_338/Solution.java
@@ -8,10 +8,30 @@ import java.util.List;
  */
 public class Solution {
     public int[] countBits(int num) {
-        int[] ans = new int[num + 1];
+        /*int[] ans = new int[num + 1];
         ans[0] = 0;
         for (int i = 1; i <= num; i++) {
             ans[i] = ans[i / 2] + i % 2;
+        }
+        return ans;*/
+        int[] ans = new int[num + 1];
+        if (num < 0) {
+            return ans;
+        }
+        ans[0] = 0;
+        int shift = 1;
+        int begin = 1;
+        int end = (1 << shift) - 1;//这里是因为移位比乘法更高效
+        while (begin <= num) {
+            for (int i = begin; i <= end; i++) {
+                if (i > num) {
+                    break;
+                }
+                ans[i] = ans[i - begin] + 1;//这个是我自己想到的动态规划方法，但是自己想歪了，没有做出来，这道题提供了一个同一个2的层级的做法
+            }
+            shift++;
+            begin = end + 1;
+            end = (1 << shift) - 1;
         }
         return ans;
     }
