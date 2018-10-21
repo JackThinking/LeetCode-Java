924ea2065e3f35017f2249f9cc409484b4c2b2b4
CitrixQian <sijieqian@foxmail.com>
2018-10-21 14:28:26 +0800

    337

--- src/medium/_337/Solution.java
+++ src/medium/_337/Solution.java
@@ -2,31 +2,45 @@ package medium._337;
 
 import structure.TreeNode;
 
-import java.util.Map;
-
 
 /**
  * Created by Citrix on 2018/10/20.
  */
 public class Solution {
+    /* public int rob(TreeNode root) {
+         if (root == null) {
+             return 0;
+         }
+         return Math.max(robNow(root), robNext(root));
+     }
+
+     public int robNow(TreeNode root) {
+         if (root == null) {
+             return 0;
+         }
+         return root.val + robNext(root.left) + robNext(root.right);
+     }
+
+     public int robNext(TreeNode root) {
+         if (root == null) {
+             return 0;
+         }
+         return rob(root.left) + rob(root.right);
+     }*/
     public int rob(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return Math.max(robNow(root), robNext(root));
+        if (root == null) return 0;
+        int[] res = dfs(root);
+        return Math.max(res[0], res[1]);
     }
 
-    public int robNow(TreeNode root) {
-        if (root == null) {
-            return 0;
+    private int[] dfs(TreeNode treeNode) {
+        if (treeNode == null) {
+            return new int[]{0, 0};
         }
-        return root.val + robNext(root.left) + robNext(root.right);
-    }
-
-    public int robNext(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return rob(root.left) + rob(root.right);
+        int[] left = dfs(treeNode.left);
+        int[] right = dfs(treeNode.right);
+        int doit = treeNode.val + left[1] + right[1];
+        int notdo = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return new int[]{doit, notdo};
     }
 }
---
+++ src/medium/_416/Solution.java
@@ -0,0 +1,10 @@
+package medium._416;
+
+/**
+ * Created by Citrix on 2018/10/20.
+ */
+public class Solution {
+    public boolean canPartition(int[] nums) {
+
+    }
+}
