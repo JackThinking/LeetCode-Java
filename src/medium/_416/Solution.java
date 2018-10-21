b7a72631dee1e915df4ade404f7f4cd03243aecc
CitrixQian <sijieqian@foxmail.com>
2018-10-21 15:06:11 +0800

    416

--- src/medium/_416/Solution.java
+++ src/medium/_416/Solution.java
@@ -5,7 +5,7 @@ package medium._416;
  */
 public class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = 0;
+        /*int sum = 0;
         for (int num : nums) {
             sum += num;
         }
@@ -24,5 +24,28 @@ public class Solution {
             }
         }
         return dp[target];
+    }*/
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        return helper(nums.length - 1, nums, target);
+    }
+
+    private boolean helper(int index, int[] nums, int remain) {
+        if (index == -1) {
+            return false;
+        }
+        if (remain == nums[index]) {
+            return true;
+        }
+        if (remain < nums[index]) {
+            return false;
+        }
+        return helper(index - 1, nums, remain - nums[index]) || helper(index - 1, nums, remain);
     }
 }
