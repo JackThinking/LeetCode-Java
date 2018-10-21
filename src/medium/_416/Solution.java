0f0285f2d378377e925edc4b87c8a76b8d04dda8
CitrixQian <sijieqian@foxmail.com>
2018-10-21 14:57:50 +0800

    416

--- src/medium/_416/Solution.java
+++ src/medium/_416/Solution.java
@@ -5,6 +5,24 @@ package medium._416;
  */
 public class Solution {
     public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        boolean dp[] = new boolean[target + 1];
+        dp[0] = true;
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = target; j >= 0; j--) {
+                if (j >= nums[i - 1]) {
+                    dp[j] = dp[j - nums[i - 1]] || dp[j];
+                }
 
+            }
+        }
+        return dp[target];
     }
 }
