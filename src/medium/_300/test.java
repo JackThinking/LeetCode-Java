a89fbd24e6c6e2034b1c3a0a612843a8a8fc892d
CitrixQian <sijieqian@foxmail.com>
2018-10-23 19:28:39 +0800

    300

--- src/medium/_300/test.java
+++ src/medium/_300/test.java
@@ -5,27 +5,22 @@ package medium._300;
  */
 public class test {
     public int lengthOfLIS(int[] nums) {
-        int size = 0;
         int n = nums.length;
-        int[] list = new int[n];
+        int size = 0;
+        int[] temp = new int[n];
         for (int num : nums) {
-            int i = 0;
-            int j = size;//为什么不是size-1
-            /*
-            * 居然是不等于
-            * */
-            while (i != j) {
-                int mid = i + (j - i) / 2;
-                if (num > list[mid]) {
-                    i = mid + 1;
-                } else
-                    j = mid;//这边也是
+            int left = 0;
+            int right = size;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (num > temp[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
             }
-            list[i] = num;
-            /*
-            * 新来的数比之前的都大，所以size扩大
-            * */
-            if (size == i) {
+            temp[left] = num;
+            if (left == size) {
                 size++;
             }
         }
