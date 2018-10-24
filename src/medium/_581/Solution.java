184365378aacfe2dd5f34459f8bbe1409fe08583
CitrixQian <sijieqian@foxmail.com>
2018-10-24 19:19:43 +0800

    91

--- src/medium/_581/Solution.java
+++ src/medium/_581/Solution.java
@@ -25,4 +25,10 @@ public class Solution {
         }
         return end - begin + 1;
     }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 6, 4, 8, 10, 9, 15};
+        System.out.println(solution.findUnsortedSubarray(nums));
+    }
 }
