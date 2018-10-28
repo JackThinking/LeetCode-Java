7aab2490544a75a5c1e9ec74358d504e3149a82d
CitrixQian <sijieqian@foxmail.com>
2018-10-28 20:58:51 +0800

    76

---
+++ src/hard/_76/Solution.java
@@ -0,0 +1,64 @@
+package hard._76;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by Citrix on 2018/10/28.
+ */
+public class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() == 0 || t.length() == 0 || s.length() < t.length()) {
+            return "";
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        int left = 0;
+        int minLeft = 0;
+        int minLen = s.length() + 1;
+        int count = 0;
+        for (int right = 0; right < s.length(); right++) {
+            if (map.containsKey(s.charAt(right))) {
+                map.put(s.charAt(right), map.get(s.charAt(right)) - 1);
+                /*
+                 * if的条件忘记了
+                 * */
+                if (map.get(s.charAt(right)) >= 0) {
+                    count++;
+                }
+                while (count == t.length()) {
+                    /*
+                    * 这边一大堆错
+                    * */
+                    if (right - left + 1 < minLen) {
+                        minLeft = left;
+                        minLen = right - left + 1;
+
+                    }
+                    if (map.containsKey(s.charAt(left))) {
+                        map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
+                        /*
+                         * 这边应该是可以去掉if的
+                         * */
+                        if (map.get(s.charAt(left)) > 0) {
+                            count--;
+                        }
+                    }
+                    left++;
+                }
+            }
+        }
+        if (minLen > s.length()) {
+            return "";
+        }
+        return s.substring(minLeft, minLeft + minLen);
+    }
+}
