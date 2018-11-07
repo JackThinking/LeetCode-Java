1468763ee7816456e617072b9c7376fadb07ab92
CitrixQian <sijieqian@foxmail.com>
2018-11-07 19:47:05 +0800

    152

--- src/hard/_152/Solution.java
+++ src/hard/_152/Solution.java
@@ -4,26 +4,31 @@ package hard._152;
  * Created by Citrix on 2018/7/10.
  */
 public class Solution {
+    /*
+     * 2钟情况考虑，一种是来的值比之前的都大了，还有一种是正负数的变换
+     * */
     public int maxProduct(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
         int n = nums.length;
-        int[] maxdp = new int[n+1];
-        int[] mindp = new int[n+1];
-        int maxVal = Integer.MIN_VALUE;
-        maxdp[0] = 1;
-        mindp[0] = 1;
-        for (int i = 1; i < n+1; i++) {
-            maxdp[i] = Math.max(maxdp[i-1]*nums[i-1], Math.max(mindp[i-1]*nums[i-1], nums[i-1]));
-            mindp[i] = Math.min(mindp[i-1]*nums[i-1], Math.min(maxdp[i-1]*nums[i-1], nums[i-1]));
-            maxVal = Math.max(maxVal, maxdp[i]);
+        int maxVal = nums[0];
+        int[] max = new int[n];
+        int[] min = new int[n];
+        max[0] = min[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0) {
+                max[i] = Math.max(nums[i], max[i - 1] * nums[i]);
+                min[i] = Math.min(nums[i], min[i - 1] * nums[i]);
+            } else {
+                max[i] = Math.max(nums[i], min[i - 1] * nums[i]);
+                min[i] = Math.min(nums[i], max[i - 1] * nums[i]);
+            }
+            maxVal = Math.max(maxVal, max[i]);
         }
         return maxVal;
     }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {2,3,-2,4};
+        int[] nums = {2, 3, -2, 4};
         System.out.println(solution.maxProduct(nums));
     }
 }
