e4a9aff380fbfcfc3934ae142b2b0f7903a3b2a2
CitrixQian <sijieqian@foxmail.com>
2018-11-27 20:17:56 +0800

    347

--- src/medium/_347/Solution.java
+++ src/medium/_347/Solution.java
@@ -1,34 +1,35 @@
 package medium._347;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Citrix on 2018/7/24.
  */
 public class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> table = new HashMap<>();
-        List<Integer>[] bukcet = new List[nums.length+1];
-        for(int n: nums){
-            table.put(n,table.getOrDefault(n,0)+1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        for(int key: table.keySet()){
-            int fqc = table.get(key);
-            if (bukcet[fqc] == null){
-                bukcet[fqc] = new ArrayList<>();
+        List<Integer>[] bucket = new List[nums.length + 1];
+        /*
+         * 按照值的大小进行排序，不熟练啊卧槽，这个和桶排序是什么关系呀
+         * */
+        for (int key : map.keySet()) {
+            int freq = map.get(key);
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList<>();
             }
-            bukcet[fqc].add(key);
+            bucket[freq].add(key);
         }
-
+        /*
+         * 按照值的大小进行弹出
+         * */
         List<Integer> res = new ArrayList<>();
-        for (int i = nums.length; i >= 0 && res.size()<k; i--) {
-           if (bukcet[i]!=null){
-               res.addAll(bukcet[i]);
-           }
-
+        for (int i = nums.length; i > 0 && res.size() < k; i++) {
+            if (bucket[i] != null) {
+                res.addAll(bucket[i]);
+            }
         }
         return res;
     }
