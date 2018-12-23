82c75b17586da80a1721e03097466dcd8dace6cd
CitrixQian <sijieqian@foxmail.com>
2018-12-23 19:29:07 +0800

    15

---
+++ src/medium/_15/Solution.java
@@ -0,0 +1,43 @@
+package medium._15;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Citrix on 2018-12-23.
+ */
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i >= 1 && nums[i] != nums[i - 1])) {
+                int lo = i + 1, hi = nums.length - 1, sum = 0 - nums[i];
+                while (lo < hi) {
+                    if (nums[lo] + nums[hi] == sum) {
+                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        while (lo < hi && nums[lo] == nums[lo + 1]) {
+                            lo++;
+                        }
+                        while (lo < hi && nums[hi] == nums[hi - 1]) {
+                            hi--;
+                        }
+                        lo++;
+                        hi--;
+                    } else if (nums[lo] + nums[hi] < sum) {
+                        lo++;
+                    } else {
+                        hi--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {-1,0,1,2,-1,-4};
+        System.out.println(solution.threeSum(nums));
+    }
+}
