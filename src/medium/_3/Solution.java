b289c0bb04dae643f6633e01a16571c91eb4738c
CitrixQian <sijieqian@foxmail.com>
2018-12-24 20:47:34 +0800

    3-update

--- src/medium/_3/Solution.java
+++ src/medium/_3/Solution.java
@@ -8,7 +8,7 @@ import java.util.Map;
  */
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0) {
+        /*if (s.length() == 0) {
             return 0;
         }
         int max = 0;
@@ -20,6 +20,33 @@ public class Solution {
             map.put(s.charAt(i), i);
             max = Math.max(max, i - j + 1);
         }
+        return max;*/
+        if (s.length() < 2) {
+            return s.length();
+        }
+        boolean[] dict = new boolean[128];
+        char[] arrays = s.toCharArray();
+        int start = 0;
+        int max = 1;
+        dict[arrays[start]] = true;
+        for (int i = 1; i < arrays.length; i++) {
+            if (dict[arrays[i]]) {
+                while (arrays[start] != arrays[i]) {
+                    dict[arrays[start]] = false;
+                    ++start;
+                }
+                ++start;
+            } else {
+                dict[arrays[i]] = true;
+            }
+            max = Math.max(max, i - start + 1);
+        }
         return max;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String nums = "abcabcbb";
+        System.out.println(solution.lengthOfLongestSubstring(nums));
     }
 }
